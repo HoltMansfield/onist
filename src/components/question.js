a800ddfbb664d6260eb7f9f8c8d6e761025d59ec
Holt Mansfield <HoltMansfield@gmail.com>
2017-08-16 18:10:22 -0400

    container to component and function to service-like thing

--- src/components/question.js
+++ src/components/question.js
@@ -1,28 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { addChild } from '../actions'
+import guidMaker from '../services/guid'
 
-let Question = ({ dispatch }) => {
-  function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
-
-  return (
+class Question extends Component {
+  render() {
+    return (
       <div>
         <span>Do you have any children?</span>
         <input type="radio" name="yesNo" value="1" onClick={ e => {
-          dispatch(addChild({ id: uuidv4() }));
+          this.props.dispatch(addChild({ id: guidMaker.make() }));
         }}/>
         <span>Yes</span>
         <input type="radio" name="yesNo" value="0" />
         <span>No</span>
       </div>
-  )
+    )
+  }
 }
 
-Question = connect()(Question)
-
-export default Question
+export default connect()(Question)
