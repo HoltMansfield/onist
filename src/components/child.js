a0992dc79eef284ed12f72235db5ab41e05f1db7
Holt Mansfield <HoltMansfield@gmail.com>
2017-08-16 18:41:29 -0400

    save and cancel button implemented

--- src/components/child.js
+++ src/components/child.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { editChild } from '../actions'
+import { editChild, deleteChild } from '../actions'
 
 class Child extends Component {
-  handleChange(value, fieldName) {
-    const child = Object.assign(this.props.child, { [fieldName]: value });
-    this.props.dispatch(editChild(child));
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.save = this.save.bind(this)
+    this.cancel = this.cancel.bind(this)
+  }
+
+  handleChange(event) {
+    this.props.child[event.target.name] = event.target.value
+  }
+
+  save() {
+    this.props.dispatch(editChild(this.props.child))
+  }
+
+  cancel() {
+    this.props.dispatch(deleteChild(this.props.child))
   }
 
   render() {
@@ -14,19 +28,21 @@ class Child extends Component {
         <div>Child</div>
         <div>
           <input type="text"
+                 name="firstName"
                  placeholder="First Name"
                  value={this.props.child.firstName}
-                 onChange={ e => {
-                   this.handleChange(e.target.value, 'firstName')
-                 }} />
+                 onChange={this.handleChange} />
         </div>
         <div>
           <input type="text"
+                 name="lastName"
                  placeholder="Last Name"
                  value={this.props.child.lastName}
-                 onChange={ e => {
-                   this.handleChange(e.target.value, 'lastName')
-                 }} />
+                 onChange={this.handleChange} />
+        </div>
+        <div>
+          <button onClick={this.cancel}>Cancel</button>
+          <button onClick={this.save}>Save</button>
         </div>
       </div>
     )
