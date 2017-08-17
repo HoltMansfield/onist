745f7bfd9bfdc669f182d1999b7116926c2d3cc8
Holt Mansfield <HoltMansfield@gmail.com>
2017-08-16 21:47:55 -0400

    refactor event handler

--- src/components/question.js
+++ src/components/question.js
@@ -4,13 +4,20 @@ import { addChild } from '../actions'
 import guidMaker from '../services/guid'
 
 class Question extends Component {
+  constructor(props) {
+    super(props)
+    this.handleYesClicked = this.handleYesClicked.bind(this)
+  }
+
+  handleYesClicked(e) {
+    this.props.dispatch(addChild({ id: guidMaker.make() }))
+  }
+
   render() {
     return (
       <div>
         <span>Do you have any children?</span>
-        <input type="radio" name="yesNo" value="1" onClick={ e => {
-          this.props.dispatch(addChild({ id: guidMaker.make() }));
-        }}/>
+        <input type="radio" name="yesNo" value="1" onClick={this.handleYesClicked}/>
         <span>Yes</span>
         <input type="radio" name="yesNo" value="0" />
         <span>No</span>
